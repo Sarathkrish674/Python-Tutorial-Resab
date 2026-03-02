2f9ea029209600562788166ce1e267df9da352cc
Sarath Krishna <sasidharansarathkrishna@gmail.com>
2026-03-02 14:31:31 +0530

    DAY 02-03-2026 for loop

--- test.py
+++ test.py
@@ -1,17 +1,8 @@
-a = []  # List for zeros
-b = []  # List for even numbers
-c = []  # List for odd numbers
+# c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+# a = []
 
-# Taking 5 inputs one by one
-num1 = int(input("Enter the first number: "))
-if num1 == 0:
-    a.append(num1)
-elif num1 % 2 == 0:
-    b.append(num1)
-else:
-    c.append(num1)
-
-
-print("List of zeros (a):", a)
-print("List of even numbers (b):", b)
-print("List of odd numbers (c):", c)
+# # Check divisibility by 7 and append to list a
+# for i in c:
+#     if i % 7 == 0:
+#         a.append(i)
+# print(f"List a (divisible by 7): {a}")
